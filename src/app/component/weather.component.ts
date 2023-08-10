e6a96c8cc7891a4400b2bf170027440cdf197534
Yenleng <yenlengggggg@gmail.com>
2023-08-10 16:13:58 +0800

    add observable to weather component

--- src/app/component/weather.component.ts
+++ src/app/component/weather.component.ts
@@ -2,6 +2,7 @@ import { Weather } from './../models/weather';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { WeatherService } from '../services/weather.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -13,6 +14,7 @@ export class WeatherComponent implements OnInit, OnDestroy {
 
   form!: FormGroup;
   city!: string;
+  subscription$!: Subscription;
 
   model = new Weather("Singappore", 0,0,0,"",0,0);
   OPEN_WEATHER_API_KEY = "476e23fe1116f4e69d2a3e68672604e1"
@@ -27,24 +29,37 @@ export class WeatherComponent implements OnInit, OnDestroy {
     // sets form value of city in city attribute
     this.city = this.form.value['city']
     console.log(this.city);
-    this.weatherSvc.getWeather(this.city, this.OPEN_WEATHER_API_KEY)
-      .then((result)=>
-      {this.model = new Weather(
-        this.city, result.main.temp,
-        result.main.pressure,
-        result.main.humidity,
-        result.weather[0].description,
-        result.wind.deg,
-        result.wind.speed
-      )})
-      .catch((error) => {
-        console.error(error)
+    // this.weatherSvc.getWeather(this.city, this.OPEN_WEATHER_API_KEY)
+    //   .then((result)=>
+    //   {this.model = new Weather(
+    //     this.city, result.main.temp,
+    //     result.main.pressure,
+    //     result.main.humidity,
+    //     result.weather[0].description,
+    //     result.wind.deg,
+    //     result.wind.speed
+    //   )})
+    //   .catch((error) => {
+    //     console.error(error)
+    //   });
+    this.subscription$ = this.weatherSvc
+      .getWeatherUsingObservable(this.city, this.OPEN_WEATHER_API_KEY)
+      .subscribe((result)=> {
+        this.model = new Weather(
+          this.city, result.main.temp,
+          result.main.pressure,
+          result.main.humidity,
+          result.weather[0].description,
+          result.wind.speed,
+          result.wind.deg
+        )
       })
+
 
   }
 
   ngOnDestroy(): void {
-
+    this.subscription$.unsubscribe();
   }
 
   ngOnInit(): void {
